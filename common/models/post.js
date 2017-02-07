0cb46eec09bdfe5fb3ac2ffae64fcd12381a41ed
David Leconte <David Leconte>
2017-02-07 14:05:25 +0100

    Added use of FBGraph node module instead of HTTP get requests

--- common/models/post.js
+++ common/models/post.js
@@ -1,5 +1,35 @@
 'use strict';
+var fbgraph = require("fbgraph");
 
 module.exports = function(Post) {
+	Post.beforeRemote("*", (context, user, next) => {
+		console.log(context);
+		let FBToken = context.req.query.fbtoken;
 
+		if(typeof FBToken == "undefined" || FBToken == "") {
+			context.res.statusCode = 400;
+			throw new Error("No FBToken specified !");
+		}
+
+		fbgraph.setAccessToken(FBToken);
+		fbgraph.get("me", { fields: "friends" }, function(error, response) {
+    		let friendsData = response.friends.data;
+    		let friendsIDS = [];
+    		friendsData.forEach((friend, index) => {
+    			friendsIDS.push(friend.id);
+    		});
+
+    		context.req.query.filter = { where: { or: [] } };
+
+			friendsIDS.forEach((friend, index) => {
+				context.req.query.filter.where.or.push({ addressedTo: friend });
+				context.req.query.filter.where.or.push({ author: friend });
+			});
+
+			console.log(context.req.query);
+			console.log(context.req.query.filter.where.or);
+
+			next();
+  		});
+	});
 };
